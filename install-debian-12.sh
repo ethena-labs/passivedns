19f859af5bb599650a05b8f70f5214c93ae93ca6
st <shane.tolmie@gmail.com>
2024-04-16 19:36:33 +0100

    Add install script for Debian 12.

---
+++ install-debian-12.sh
@@ -0,0 +1,13 @@
+# Install passivedns on Debian 12 (Bookworm).
+sudo apt install git build-essential libldns-dev libpcap-dev automake autoconf
+autoreconf --install
+./configure
+make
+# To run in daemon mode:
+# passivedns -i ens5 -l /var/log/passivedns.log -D
+# And test using something like curl www.example.com:
+# tail -f /var/log/passivedns.log
+# 1713291888.580818||10.16.34.183||10.16.32.2||IN||www.1234.com.||CNAME||redirect.telstra.net.||300||1
+# 1713291888.580818||10.16.34.183||10.16.32.2||IN||redirect.telstra.net.||A||203.36.190.9||30||1
+# 1713291898.171922||10.16.34.183||10.16.32.2||IN||www.example.com.||AAAA||2606:2800:220:1:248:1893:25c8:1946||300||1
+# 1713291898.179097||10.16.34.183||10.16.32.2||IN||www.example.com.||A||93.184.216.34||300||1
